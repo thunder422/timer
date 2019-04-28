43d2ecd3a2f5823cf459d13e58061a1879e82244
Thunder422 <thunder422@verizon.net>
2019-04-28 13:48:02 -0400

    changed to standalone program using cmake to build
    Started by creating a Qt Quick Application - Empty with Cmake as the
    build system.  CMakeLists.txt was modified to use C++14 and the
    "-fno-sized_deallocation" option was needed to run correctly.  The
    generated main.cpp was only cleaned up for formatting.
    
    The qml.qrc file lists the resources of the program, which includes all
    of the .qml files needed by the program.  It also lists other resource
    files that get embedded into the final program, for instance the times
    up audio file needed when the timer times out.
    
    The .gitignore was updated to the auto generated one plus an entry for
    the build directory.  The previous .qmlproject file was removed.

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/* vim:ts=4:sw=4:et:sts=4:
+ *
+ * Copyright 2019 Thunder422.  All rights reserved.
+ * Distributed under GNU General Public License Version 3
+ * (See accompanying file LICENSE or <http://www.gnu.org/licenses/>)
+ */
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/timer.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl) {
+                QCoreApplication::exit(-1);
+            }
+        }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
